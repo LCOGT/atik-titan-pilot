c2f777a793b2431addd539f95cb4506718989234
Steve Pandich <spandich@lcogt.net>
2014-09-02 07:29:49 -0700

    fits round 2

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ import os
 base_dir = os.path.dirname(os.path.realpath(__file__))
 
 module_artemishsc = Extension("_artemishsc", ["artemishsc.i", "ArtemisHscAPI.cpp"],
-                                include_dirs = [r"c:\Python27\Lib\site-packages\numpy\core\include"],
-                                library_dirs = [
-                                    r"c:\Python27\libs",
-                                    r"{base_dir!s}\shared".format(**locals()),
-                                    r"c:\Program Files (x86)\mingw\mingw32\i686-w64-mingw32\lib"],
-                                libraries = ["ArtemisHSC", "oleaut32"])
+                              include_dirs=[r"c:\Python27\Lib\site-packages\numpy\core\include"],
+                              library_dirs=[
+                                  r"c:\Python27\libs",
+                                  r"{base_dir!s}\shared".format(**locals()),
+                                  r"c:\Program Files (x86)\mingw\mingw32\i686-w64-mingw32\lib"],
+                              libraries=["ArtemisHSC", "oleaut32"])
 
-setup(name = "Atik Titan API", version="1.0", ext_modules = [module_artemishsc])
+setup(name="Atik Titan API", version="1.0", ext_modules=[module_artemishsc], requires=['pyfits', 'numpy'])
